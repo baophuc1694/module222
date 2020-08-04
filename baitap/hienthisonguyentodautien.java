7e7b1b9cf36928f05d9fb66164df489b19bd1ced
baophuc1694 <phucnguyenty1694@gmail.com>
2020-08-04 14:26:23 +0700

    baitap01

---
+++ baitap/hienthisonguyentodautien.java
@@ -0,0 +1,25 @@
+package baitap;
+
+public class hienthisonguyentodautien {
+    public static boolean primeCheck(int a){
+        for (int i = 2; i <= Math.sqrt(a); i++){
+            if (a % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int num = 20;
+        int count = 0;
+        int N = 2;
+        while (count < num){
+            if (primeCheck(N)){
+                count++;
+                System.out.println(N);
+            }
+            N++;
+        }
+    }
+}
---
+++ baitap/mottramsonguyentodautien.java
@@ -0,0 +1,21 @@
+package baitap;
+
+public class mottramsonguyentodautien {
+    public static boolean primeCheck(int a){
+        for (int i = 2; i < Math.sqrt(a); i++){
+            if (a % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        for (int i = 2; i <100; i++){
+            if (primeCheck(i)){
+                System.out.println(i + "\n");
+            }
+        }
+
+    }
+}
